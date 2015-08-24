0307c60032959f774b31685433114ced16c2ddab
PontusWirsching <flash13records@gmail.com>
2015-08-24 17:34:50 +0200

    Added DwarvenKnight in AIUnits

--- config/AIUnits.js
+++ config/AIUnits.js
@@ -15,5 +15,22 @@ AIUnits = {
         height: 32,
         xOffset: 0,
         yOffset: 20
+    },
+    DwarvenKnight : {
+        type: "ai_monster",
+        Name: "DwarvenKnight",
+        Cost: 10,
+        Size: GameTypes.Sizes.Small,
+        Faction: GameTypes.Faction.Enemy,
+        MovementSpeed: GameTypes.Speeds.Medium,
+        AttackSpeed: GameTypes.Speeds.Medium,
+        ArmorTypes: GameTypes.ArmorTypes.Light,
+        AttackTypes: [GameTypes.AttackTypes.Ranged],
+        AdvantageTo: [],
+        DisadvantageTo: [],
+        width: 32,
+        height: 32,
+        xOffset: 0,
+        yOffset: 20
     }
 };
